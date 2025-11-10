6439b3423b1a5b8ecfacc8cd9d52963ccedde7cc
rb-bhuro <omrangani99@gmail.com>
2025-11-10 11:49:20 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { DataProvider } from "./context/DataContext"; // ✅ Add this import
 
-// Attach the app to the HTML root div
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </React.StrictMode>
+console.log("✅ React loaded!");
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <h1 style={{ color: "white", textAlign: "center", marginTop: "40vh" }}>
+    React is working!
+  </h1>
 );
