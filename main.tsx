580199c9473ccf74746913867ccfa08fce6e0c82
rb-bhuro <omrangani99@gmail.com>
2025-11-10 11:46:01 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { DataProvider } from "./context/DataContext"; // ✅ Add this import
 
 // Attach the app to the HTML root div
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>
 );
